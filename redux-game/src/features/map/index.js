29f6a7fc398c6bc725458be63d0d26486661d835
marjika <bluegoo5@hotmail.com>
2018-11-29 16:08:27 -0700

    with working player movement and secret path

--- redux-game/src/features/map/index.js
+++ redux-game/src/features/map/index.js
@@ -8,12 +8,14 @@ function getTileSprite(type) {
     switch(type) {
         case 0:
             return "grass"
+        case 3:
+            return "tree"
+        case 4:
+            return "chest"
         case 5:
             return "rock"
         case 6:
             return "tree"
-        case 8:
-            return "chest"
     }
 }
 
@@ -49,7 +51,7 @@ function Map(props) {
                 top: "0px",
                 left: "0px",
                 width: "800px",
-                height: "400px",
+                height: "480px",
                 backgroundColor: "green",
                 border: "4px solid white"
             }}        
--- redux-game/src/features/player/reducer.js
+++ redux-game/src/features/player/reducer.js
@@ -1,7 +1,8 @@
 const initialState = {
     position: [0, 0],
     spriteLocation: "40px 40px",
-    direction: "east"
+    direction: "east", 
+    walkIndex: 0
 }
 
 const playerReducer = (state=initialState, action) => {
