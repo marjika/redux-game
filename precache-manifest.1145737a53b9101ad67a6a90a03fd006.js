248ca15e8703ce2e2a3e967ecc34649dbaae6656
marjika <bluegoo5@hotmail.com>
2018-11-30 19:34:56 -0700

    Updates

---
+++ precache-manifest.1145737a53b9101ad67a6a90a03fd006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "080f7de25752d806379eb64323ae4ae2",
+    "url": "/redux-game/static/media/sprite-sheet.080f7de2.png"
+  },
+  {
+    "revision": "c949e0a53a673aef883a",
+    "url": "/redux-game/static/js/runtime~main.c949e0a5.js"
+  },
+  {
+    "revision": "14c22aa456d39288aae6",
+    "url": "/redux-game/static/js/main.14c22aa4.chunk.js"
+  },
+  {
+    "revision": "edbed080d6c0a2378e0c",
+    "url": "/redux-game/static/js/1.edbed080.chunk.js"
+  },
+  {
+    "revision": "14c22aa456d39288aae6",
+    "url": "/redux-game/static/css/main.50ee8c0e.chunk.css"
+  },
+  {
+    "revision": "8f0334bdf7cf0ada0f135ecb1cc975a3",
+    "url": "/redux-game/index.html"
+  }
+];
